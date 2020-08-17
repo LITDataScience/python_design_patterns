63bd1f9101b1991a61387e1957c06a114f4a5d40
Rajeev Sharma <rajeevs@nvidia.com>
2020-08-17 16:45:14 +0530

    Updated v1.1

--- single_responsibility_principle.py
+++ single_responsibility_principle.py
@@ -14,13 +14,27 @@ class Journal:
         del self.entries[position]
 
     """Display the data in a human readable form"""
-
     def __str__(self):
         return '\n'.join(self.entries)
+
+
+class PersistenceManager:
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
 
 
 j = Journal()
 j.add_entry('I woke up happy today')
 j.add_entry('Human brain is a separate organism')
 j.add_entry('The human brain is trying to go out in space to reach the planet it came from {=_=} ')
-print(f'Journal entries: \n{j}')
+# print(f'Journal entries: \n{j}')
+
+file = r'C:\Users\rajeevs\Documents\python_design_patterns\temp.txt'
+PersistenceManager.save_to_file(j, file)
+
+# Verifying the journal
+with open(file) as fh:
+    print(fh.read())
